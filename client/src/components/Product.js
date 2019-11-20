2c089eee56ece1043795bc361716c23152cdb18a
Bowen Burnett-Bride <56246909+bowenbride@users.noreply.github.com>
2019-11-20 15:03:48 -0800

    Tablet styling done, moving on to finish responsiveness then power through backend

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -12,40 +12,42 @@ export default class Product extends Component {
           <h1 className="product__name texthead">Product Name</h1>
         </div>
         <button className="product__stock">In Stock</button>
-        <div className="product__textwrap">
-          <h3 className="product__texthead label">ITEM DESCRIPTION</h3>
-          <p className="product__textbody para">
-            Here is a more detailed summary of the product name, it’s uses,
-            industries and possible attributes that could be used to describe
-            the product.
-          </p>
-        </div>
-        <div className="product__textwrap">
-          <h3 className="product__texthead label">ORDERED BY</h3>
-          <p className="product__textbody para">Mark Saunders</p>
-        </div>
-        <div className="product__textwrap">
-          <h3 className="product__texthead label">REFERENCE NUMBER</h3>
-          <p className="product__textbody para">JK2020FD7811201</p>
-        </div>
-        <div className="product__textwrap">
-          <h3 className="product__texthead label">LAST ORDERED</h3>
-          <p className="product__textbody para">5/24/2018</p>
-        </div>
-        <div className="product__textwrap">
-          <h3 className="product__texthead label">LOCATION</h3>
-          <p className="product__textbody para">Toronto, CAN</p>
-        </div>
-        <div className="product__textwrap">
-          <h3 className="product__texthead label">QUANTITY</h3>
-          <p className="product__textbody para">12000</p>
-        </div>
-        <div className="product__textwrap">
-          <h3 className="product__texthead label">CATEGORIES</h3>
-          <p className="product__textbody para">
-            Industrial, Automotive, Heavy, Mechanical, Engineering,
-            Transportation, Sales
-          </p>
+        <div className="product__textcontainer">
+          <div className="product__description">
+            <h3 className="product__texthead label">ITEM DESCRIPTION</h3>
+            <p className="product__textbody para">
+              Here is a more detailed summary of the product name, it’s uses,
+              industries and possible attributes that could be used to describe
+              the product.
+            </p>
+          </div>
+          <div className="product__textwrap">
+            <h3 className="product__texthead label">ORDERED BY</h3>
+            <p className="product__textbody para">Mark Saunders</p>
+          </div>
+          <div className="product__textwrap">
+            <h3 className="product__texthead label">REFERENCE NUMBER</h3>
+            <p className="product__textbody para">JK2020FD7811201</p>
+          </div>
+          <div className="product__textwrap">
+            <h3 className="product__texthead label">LAST ORDERED</h3>
+            <p className="product__textbody para">5/24/2018</p>
+          </div>
+          <div className="product__textwrap">
+            <h3 className="product__texthead label">LOCATION</h3>
+            <p className="product__textbody para">Toronto, CAN</p>
+          </div>
+          <div className="product__textwrap">
+            <h3 className="product__texthead label">QUANTITY</h3>
+            <p className="product__textbody para">12000</p>
+          </div>
+          <div className="product__categories">
+            <h3 className="product__texthead label">CATEGORIES</h3>
+            <p className="product__textbody para">
+              Industrial, Automotive, Heavy, Mechanical, Engineering,
+              Transportation, Sales
+            </p>
+          </div>
         </div>
         <button className="product__edit">EDIT</button>
       </div>
