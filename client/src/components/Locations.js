e42296d57eab56853453848a39523a80e1a6eb34
jennormous <smj.boudreau@gmail.com>
2019-11-21 01:05:14 -0800

    Partially completed the mobile styling for this component. Styles were modified from Inventory.js for consistency in code.

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import arRight from "../assets/Icons/SVG/Icon-arrow-right.svg";
 
 export default class Locations extends Component {
@@ -7,31 +7,42 @@ export default class Locations extends Component {
       this.props.locations &&
       this.props.locations.map(function(location) {
         return (
-          <div>
-            <div>
-              <h3>{location.warehouse}</h3>
-              <h4>{location.location}</h4>
-              <img src={arRight} />
+          <div className="locations">
+            <div className="locations__loc">
+              <div className="locations__loc--text">
+                <h3 className="subheader">{location.warehouse}</h3>
+                <h4 className="para">{location.location}</h4>
+              </div>
+              <img className="locations__loc--img" src={arRight} />
             </div>
-            <div>
-              <h3>{location.contact}</h3>
-              <h3>{location.title}</h3>
+            <div className="locations__person">
+              <h3 className="para">{location.contact}</h3>
+              <h3 className="para">{location.title}</h3>
             </div>
-            <div>
-              <h3>{location.phone}</h3>
-              <h3>{location.email}</h3>
+            <div className="locations__contact">
+              <h3 className="para">{location.phone}</h3>
+              <h3 className="para">{location.email}</h3>
             </div>
-            <div>
-              <h3>{location.categories}</h3>
+            <div className="locations__cat">
+              <h3 className="para">{location.categories}</h3>
             </div>
           </div>
         );
       });
 
     return (
-      <div>
-        <h1>Locations</h1>
-        <input type="text"></input>
+      <div className="locations">
+        <div className="locations__top-section">
+          <h1 className="locations__top-section--title texthead">Locations</h1>
+          <form onKeyUp>
+            <input
+              type="text"
+              name="searchbar"
+              placeholder="Search"
+              className="locations__top-section--search"
+            />
+          </form>
+        </div>
         {locationInfo}
       </div>
     );
