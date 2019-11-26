380add03a8f992c22c9e96f2f40eb90a824b5b0b
Bowen Burnett-Bride <56246909+bowenbride@users.noreply.github.com>
2019-11-26 11:34:40 -0800

    Routing about halfway done

--- client/src/App.js
+++ client/src/App.js
@@ -5,165 +5,32 @@ import Header from "./components/Header";
 import Inventory from "./components/Inventory";
 import Locations from "./components/Locations";
 import Warehouse from "./components/locations/Warehouse";
+import ProductSum from "./components/inventory/ProductSum";
 import { Route, Switch } from "react-router-dom";
+import axios from "axios";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      warehouses: [
-        {
-          id: "1",
-          warehouse: "Bowen's Place",
-          country: "Canada",
-          province: "BC",
-          city: "Vancouver",
-          postal: "V6J 7K8",
-          address: "123 Main street",
-          items: [
-            {
-              product: "Beyblade",
-              description: "Its a beyblade what more do you need to know",
-              ordered: "12/12/12",
-              quantity: "1,000,000",
-              id: "11",
-              status: "In Stock"
-            },
-            {
-              product: "Big gun",
-              description: "its really big",
-              ordered: "12/1/19",
-              quantity: "1",
-              id: "12",
-              status: "In Stock"
-            },
-            {
-              product: "Old man and the Sea",
-              description: "decent book but we have way too many of them",
-              ordered: "1/22/99",
-              quantity: "12,000,000",
-              id: "13",
-              status: "In Stock"
-            }
-          ]
-        },
-        {
-          id: "2",
-          warehouse: "Jen's goodies",
-          country: "Canada",
-          province: "BC",
-          city: "Vancouver",
-          postal: "V6J 7K8",
-          address: "123 Main street",
-          items: [
-            {
-              product: "Shiba Inu",
-              description: "Fluffy hyper doggo",
-              ordered: "3/14/18",
-              quantity: "10",
-              id: "21",
-              status: "In Stock"
-            },
-            {
-              product: "Dolphin",
-              description: "We will not sell to Seaworld",
-              ordered: "7/4/18",
-              quantity: "14",
-              id: "22",
-              status: "In Stock"
-            },
-            {
-              product: "really shap kives",
-              description: "its very sharp and we have a lot watch out Alyssa ",
-              ordered: "11/19/19",
-              quantity: "12,000,000",
-              id: "23",
-              status: "In Stock"
-            }
-          ]
-        },
-        {
-          id: "3",
-          warehouse: "Samuel's Dump",
-          country: "Canada",
-          province: "BC",
-          city: "Vancouver",
-          postal: "V6J 7K8",
-          address: "123 Main street",
-          items: [
-            {
-              product: "Used samich",
-              description: "its not as bad as it sounds just plug your nose",
-              ordered: "10/1/11",
-              quantity: "7.5",
-              id: "31",
-              status: "In Stock"
-            },
-            {
-              product: "Sombody's Nan",
-              description:
-                "She's real sweet just needs a good bath and she's good as new",
-              ordered: "8/4/19",
-              quantity: "1",
-              id: "32",
-              status: "In Stock"
-            },
-            {
-              product: "Nike",
-              description: "They brand new come get em",
-              ordered: "3/25/10",
-              quantity: "12,000,000",
-              id: "33",
-              status: "In Stock"
-            }
-          ]
-        }
-      ],
-      locations: [
-        {
-          id: "1",
-          warehouse: "Bowen's Place",
-          country: "Canada",
-          province: "BC",
-          city: "Vancouver",
-          postal: "V6J 7K8",
-          address: "123 Main street",
-          contact: "George dubyuh",
-          title: "Prezdent",
-          phone: "1(800) plz-vote",
-          email: "george@texas.com",
-          categories: "Toys, Guns, Chew, Books"
-        },
-        {
-          id: "2",
-          warehouse: "Jen's goodies",
-          country: "Canada",
-          province: "BC",
-          city: "Vancouver",
-          postal: "V6J 7K8",
-          address: "123 Main street",
-          contact: "Daddy Mattias",
-          title: "Sugar Baby",
-          phone: "1(800) shiba",
-          email: "jensad@gmail.com",
-          categories: "Dogs, all aminals, knives, gin & tonics"
-        },
-        {
-          id: "3",
-          warehouse: "Samuel's Dump",
-          country: "Canada",
-          province: "BC",
-          city: "Vancouver",
-          postal: "V6J 7K8",
-          address: "123 Main street",
-          contact: "Codee Palmer",
-          title: "Garbage Boy",
-          phone: "1(800) garbage",
-          email: "garbage@junkmail.com",
-          categories: "Trash, Grandma, Shoes, semi clean socks"
-        }
-      ]
+      warehouses: [],
+      locations: []
     };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/location").then(result => {
+      console.log(result);
+      this.setState({ locations: result.data });
+    });
+    axios.get("http://localhost:5000/warehouse").then(result => {
+      console.log(result);
+      this.setState({ warehouses: result.data });
+    });
+    axios.get("http://localhost:5000/product").then(result => {
+      console.log(result);
+      this.setState({ warehouses: result.data });
+    });
   }
 
   render() {
@@ -190,8 +57,15 @@ export default class App extends Component {
 
           <Route
             path="/warehouse"
-            render={props => <Warehouse locations={this.state.locations} />}
+            render={props => (
+              <Warehouse
+                locations={this.state.locations}
+                warehouses={this.state.warehouses}
+              />
+            )}
           />
+
+          <Route path="/product" render={props => <ProductSum />} />
         </Switch>
       </div>
     );
---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+// Define consts for routing
+const Location = require("./model/location.json");
+const Warehouse = require("./model/warehouses.json");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/location", (req, res, next) => {
+  res.send(JSON.stringify(Location));
+});
+
+app.get("/warehouse", (req, res, next) => {
+  res.send(JSON.stringify(Warehouse));
+});
+
+app.get("/product/:id", (req, res, next) => {
+  console.log(req.params.id);
+  const Product = Warehouse.map((obj, index) => {
+    return obj["items"].filter(item => item.id === req.params.id);
+  });
+
+  let FixedProd = Product.filter(prodElement => prodElement.length > 0);
+  res.send(JSON.stringify(FixedProd));
+});
+
+app.get("/warehouse/:id", (req, res, next) => {
+  console.log(req.params.id);
+  const WarehouseOut = Warehouse.filter(obj => obj["id"] === req.params.id);
+  res.send(JSON.stringify(WarehouseOut));
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
