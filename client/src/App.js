9f8b4b510b8d42565b87c33ad3ae27247f90f994
Bowen Burnett-Bride <56246909+bowenbride@users.noreply.github.com>
2019-11-26 16:57:15 -0800

    Additional routing complete, Edit Product page in progress

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Warehouse from "./components/locations/Warehouse";
 import ProductSum from "./components/inventory/ProductSum";
 import { Route, Switch } from "react-router-dom";
 import axios from "axios";
+import ProductEdit from "./components/inventory/ProductEdit";
 
 export default class App extends Component {
   constructor(props) {
@@ -71,6 +72,13 @@ export default class App extends Component {
             path="/product/:id"
             render={props => (
               <ProductSum data={this.state.products} {...props} />
+            )}
+          />
+
+          <Route
+            path="/product/edit"
+            render={props => (
+              <ProductEdit data={this.state.products} {...props} />
             )}
           />
         </Switch>
--- client/src/components/inventory/ProductEdit.js
+++ client/src/components/inventory/ProductEdit.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Route, Switch, Link } from "react-router-dom";
 
-export default class Product extends Component {
+export default class ProductEdit extends Component {
   constructor(props) {
     super(props);
 
@@ -46,47 +45,43 @@ export default class Product extends Component {
           <div className="product__notdescription">
             <div className="product__textwrap">
               <h3 className="product__texthead label">ORDERED BY</h3>
-              <p className="product__textbody para">
+              <input type="text" className="product__textbody para">
                 {this.state.products.orderedBy}
-              </p>
+              </input>
             </div>
             <div className="product__textwrap">
               <h3 className="product__texthead label">REFERENCE NUMBER</h3>
-              <p className="product__textbody para">
+              <input type="text" className="product__textbody para">
                 {this.state.products.reference}
-              </p>
+              </input>
             </div>
             <div className="product__textwrap">
               <h3 className="product__texthead label">LAST ORDERED</h3>
-              <p className="product__textbody para">
+              <input type="text" className="product__textbody para">
                 {this.state.products.ordered}
-              </p>
+              </input>
             </div>
             <div className="product__textwrap">
               <h3 className="product__texthead label">LOCATION</h3>
-              <p className="product__textbody para">
+              <input type="text" className="product__textbody para">
                 {this.state.products.location}
-              </p>
+              </input>
             </div>
             <div className="product__textwrap">
               <h3 className="product__texthead label">QUANTITY</h3>
-              <p className="product__textbody para">
+              <input type="text" className="product__textbody para">
                 {this.state.products.quantity}
-              </p>
+              </input>
             </div>
             <div className="product__categories">
               <h3 className="product__texthead label">CATEGORIES</h3>
-              <p className="product__catbody para">
+              <input type="text" className="product__catbody para">
                 {this.state.products.categories}
-              </p>
+              </input>
             </div>
           </div>
         </div>
-        <Switch>
-          <Link to="/product/edit">
-            <button className="product__edit">EDIT</button>
-          </Link>
-        </Switch>
+        <button className="product__edit">EDIT</button>
       </div>
     );
   }
