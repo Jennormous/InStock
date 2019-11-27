5cc86d563b351491776d2116e1b2fa784d62b74a
jennormous <smj.boudreau@gmail.com>
2019-11-26 23:02:17 -0800

    change nav links to hover

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,8 +8,8 @@ export default class Header extends Component {
       <header className="header header__wrapper">
         <img className="header__img" src={logo} alt="logo" />
         <ul className="header__list">
-          <Link to="/">
-            <li className="header__list--item active">Inventory</li>{" "}
+          <Link className="header__list--item" to="/">
+            <li>Inventory</li>{" "}
           </Link>
           <Link to="/locations">
             <li className="header__list--item">Locations</li>
